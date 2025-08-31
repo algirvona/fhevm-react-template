a29e6315cc4d9efdf044f3b07b73555985314cee
duyenhtm <53867244+algirvona@users.noreply.github.com>
2025-08-31 08:48:37 +0000

    your commit message

--- packages/site/abi/FHECounterAddresses.ts
+++ packages/site/abi/FHECounterAddresses.ts
@@ -4,6 +4,6 @@
   Command: 'npm run genabi'
 */
 export const FHECounterAddresses = { 
-  "11155111": { address: "0x0000000000000000000000000000000000000000", chainId: 11155111, chainName: "sepolia" },
-  "31337": { address: "0x7553CB9124f974Ee475E5cE45482F90d5B6076BC", chainId: 31337, chainName: "hardhat" },
+  "11155111": { address: "0x7DEAA79D99d061bD93f7361dc07d2e38965952cd", chainId: 11155111, chainName: "sepolia" },
+  "31337": { address: "0x5FbDB2315678afecb367f032d93F642f64180aa3", chainId: 31337, chainName: "hardhat" },
 };
